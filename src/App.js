9a2ca8bde268eb543d6f5dd33476b416bd38aa02
Malcolm Scruggs <malcolm.scruggs@appfolio.com>
2021-06-29 15:44:25 -0400

    Revert "Updates"
    This reverts commit 6dcbd04c

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import {
+  createClient,
+  dedupExchange,
+  fetchExchange,
+  Provider
+} from "urql";
+
+import { requestPolicyExchange } from "@urql/exchange-request-policy";
+import { offlineExchange } from "@urql/exchange-graphcache";
+import { makeDefaultStorage } from "@urql/exchange-graphcache/default-storage";
+import { Todos } from "./components";
+
+const storage = makeDefaultStorage({
+  idbName: "graphcache-v3", // The name of the IndexedDB database
+  maxAge: 7 // The maximum age of the persisted data in days
+});
+
+const cache = offlineExchange({
+  storage
+});
+
+const client = createClient({
+  url: "https://0ufyz-4000.sse.codesandbox.io",
+  exchanges: [
+    dedupExchange,
+    requestPolicyExchange({
+      ttl: 2 * 1000,
+      shouldUpgrade: (op) => {
+        console.log('upgrading', op);
+        return true;
+      }
+    }),
+    cache,
+    fetchExchange
+  ],
+  requestPolicy: "cache-first"
+});
+
+const App = () => {
+  const [showTodos, setShowTodos] = useState(false);
+  const toggleTodos = () => {
+    setShowTodos((previousValue) => !previousValue);
+  };
+
+  return (
+    <Provider value={client}>
+      <main>
+        <button onClick={toggleTodos}>Toggle todos</button>
+        {showTodos ? <Todos /> : null}
+      </main>
+    </Provider>
+  );
+};
+
+export default App;
---
+++ src/components/Todos.js
@@ -0,0 +1,45 @@
+import React, { useCallback, useMemo } from "react";
+import gql from "graphql-tag";
+import { useQuery } from "urql";
+import { Loading, Error, Todo } from ".";
+
+export const Todos = () => {
+  const [res, executeQuery] = useQuery({ query: TodoQuery });
+
+  const todos = useMemo(() => {
+    if (res.fetching || !res.data) {
+      return <Loading />;
+    }
+
+    if (res.error) {
+      return <Error>{res.error.message}</Error>;
+    }
+
+    return (
+      <ul>
+        {res.data.todos.map((todo) => (
+          <Todo key={todo.id} {...todo} />
+        ))}
+      </ul>
+    );
+  }, [res]);
+
+  return (
+    <>
+      <h1>Todos</h1>
+      {todos}
+    </>
+  );
+};
+
+Todos.displayName = "Todos";
+
+const TodoQuery = gql`
+  query {
+    todos {
+      id
+      text
+      complete
+    }
+  }
+`;
